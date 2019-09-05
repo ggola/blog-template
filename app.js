6108f5a1a8e96d8a2d2eb06be3524f27a51ae150
giuliogola <ggola80@gmail.com>
2019-09-05 17:47:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,97 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require('lodash');
+const Sugar = require('sugar');
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// CONNECT MONGOOSE with url of mongo at atlas
+mongoose.connect("mongodb://localhost:27017/myblogDB", {useNewUrlParser: true});
+
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+let posts = [];
+
+app.get("/", function(req, res) {
+
+  Post.find({}, function(err, foundPosts){
+    if (!err) {
+      const truncatedPosts = [];
+      foundPosts.forEach(function(post){
+        const sugarContent = new Sugar.String(post.body);
+        const truncatedContent = sugarContent.truncate(100);
+        const truncatedPost = {
+          _id: post._id,
+          title: post.title,
+          body: truncatedContent
+        }
+        truncatedPosts.push(truncatedPost);
+      })
+      res.render('home', {homeStartingContent: homeStartingContent, posts: truncatedPosts});
+    } else {
+      console.log("Error loading posts");
+    }
+  })
+
+});
+
+app.get("/about", function(req, res) {
+  res.render('about', {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+  res.render('contact', {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res) {
+  res.render('compose');
+});
+
+app.get('/posts/:postId', function (req, res) {
+  const postId = req.params.postId;
+  Post.findById(postId, function(err, foundPost){
+    if (!err) {
+      res.render('post', {postTitle: foundPost.title, postBody: foundPost.body});
+    } else {
+      console.log("Error retrieving post");
+    }
+  });
+})
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody
+  });
+  post.save(function(err){
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started");
+});
